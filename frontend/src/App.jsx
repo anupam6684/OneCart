38a44ee82ce06aa0a97b2e850adce094c83aa49c
Anupam Jana <anupamjana6684@gmail.com>
2026-01-23 21:20:45 +0530

    navber is done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,24 +13,19 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
-      <Nav />
-
-      <div className="container-fluid px-0">
-        <div className="container py-3">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/orders" element={<Orders />} />
-            <Route path="/placeorder" element={<PlaceOrder />} />
-            <Route path="/product/:id" element={<Product />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div>
+      <div className=" custom-padding">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/placeorder" element={<PlaceOrder />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </div>
-
-      <Footer />
     </>
   );
 }
