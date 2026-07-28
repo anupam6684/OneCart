23c108936cda9269de175141b030bf125abd91f2
ANUPAM JANA <anupamjana6684@gmail.com>
2026-07-28 22:51:25 +0530

    Update mongodb.js

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,31 +1,15 @@
-import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-import dns from "dns";
 
-dotenv.config();
-
-dns.setServers(["8.8.8.8", "1.1.1.1"]);
-
-const app = express();
-
-app.use(express.json());
-
-const connectDB = async () => {
+const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 5000,
-    });
+    await mongoose.connect(process.env.MONGO_URL);
+    dbName: "onecart";
 
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-    console.log(`📂 Database: ${conn.connection.name}`);
-
+    console.log("✅ Connected to MongoDB!");
   } catch (error) {
-    console.error("❌ MongoDB Connection Failed");
-    console.error(error.message);
+    console.error("❌ MongoDB Connection Error:", error.message);
     process.exit(1);
   }
 };
 
-export default connectDB;
-
+export default connectDb;
